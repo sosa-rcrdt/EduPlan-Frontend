ca9778ddca28480aee409564bf0ff98440a17399
Nelson <sosafco3@gmail.com>
2025-12-02 18:30:23 -0600

    Creación Del Landing Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { LoginScreenComponent } from './screens/login-screen/login-screen.compon
 import { HomeScreenComponent } from './screens/home-screen/home-screen.component';
 import { RegistroScreenComponent } from './screens/registro-screen/registro-screen.component';
 import { CambiarContrasenaScreenComponent } from './screens/cambiar-contrasena-screen/cambiar-contrasena-screen.component';
+import { LandingPageScreenComponent } from './screens/landing-page-screen/landing-page-screen.component';
 
 const routes: Routes = [
   //Pantalla principal del login
-  { path: '', component: LoginScreenComponent, pathMatch: 'full' },
+  { path: '', component: LandingPageScreenComponent, pathMatch: 'full' },
+  { path: 'login', component: LoginScreenComponent, pathMatch: 'full'},
   { path: 'registro', component: RegistroScreenComponent, pathMatch: 'full' },
   { path: 'cambiar-contrasena', component: CambiarContrasenaScreenComponent, pathMatch: 'full' },
   { path: 'home', component: HomeScreenComponent, pathMatch: 'full' },
